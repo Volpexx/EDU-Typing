38cbdcf0fa7e2e4ab7ceeb26d9c8c172bdb99d08
Volpex <72867985+Volpexx@users.noreply.github.com>
2021-01-26 13:40:06 -0500

    Update reader.py

--- reader.py
+++ reader.py
@@ -7,27 +7,22 @@ from PIL import ImageGrab
 
 
 def imToString():
-
-
-
-
+	
 	pytesseract.pytesseract.tesseract_cmd ='C:/Program Files/Tesseract-OCR/tesseract.exe'
 	while(True): 
 
-	
-		cap = ImageGrab.grab(bbox =(490, 136, 1400, 175)) 
+		#defines positions to look for characters
+		capture = ImageGrab.grab(bbox =(490, 136, 1400, 175)) 
 
-		
+		#waits for esc key to be pressed for next line
 		kb.wait('esc')
+
+		#checks for words in english and sets capture to grayscale for easier processing
 		tesstr = pytesseract.image_to_string( 
-				cv2.cvtColor(nm.array(cap), cv2.COLOR_BGR2GRAY), 
+				cv2.cvtColor(nm.array(capture), cv2.COLOR_BGR2GRAY), 
 				lang ='eng') 
 		kb.write(tesstr)
 		print(tesstr)
 
-
-
 # Calling the function 
 imToString() 
-
-
